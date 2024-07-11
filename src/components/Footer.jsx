71a38dbc88d218d68a8994a8d66b655ad488fc65
malavicasurakanti <144533175+malavicasurakanti@users.noreply.github.com>
2024-07-11 15:35:11 +0200

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  AiFillYoutube,
+  AiFillInstagram,
+  AiFillHeart,
+} from "react-icons/ai";
+import { FaFacebook } from "react-icons/fa";
+
+import { images } from "../constants";
+
+const Footer = () => {
+  return (
+    <section className="bg-dark-hard">
+      <footer className="container mx-auto grid grid-cols-30 px-5 py-10 gap-y-10 gap-x-10 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10">
+        <div className="col-span-6 md:order-first lg:col-span-2">
+          <h3 className="text-[#0f0f10] font-bold md:text-lg">Products</h3>
+          <ul className="text-[#0f0f10] text-sm mt-5 space-y-4 md:text-base">
+            <li>
+              <a href="/">Portfolio Ideas</a>
+            </li>
+            <li>
+              <a href="/">Notion Templates</a>
+            </li>
+            <li>
+              <a href="/">Articles</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-6 md:col-span-4 md:col-start-4 lg:col-start-auto lg:col-span-4">
+          <h3 className="text-[#0f0f10] font-bold md:text-lg">Company</h3>
+          <ul className="text-[#0f0f10] text-sm mt-5 space-y-4 md:text-base">
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Terms</a>
+            </li>
+            <li>
+              <a href="/">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="/">Referral Program</a>
+            </li></ul>
+        </div>
+        <div className="col-span-10  md:col-span-4 lg:col-span-2">
+          <img
+            src={images.Logo}
+            alt="logo"
+            className="brightness-0 invert mx-auto md:mx-0"
+          />
+          <p className="text-sm text-[#0f0f10] text-center mt-4 md:text-left md:text-base lg:text-sm">
+            Place to Explore Ideas
+          </p>
+          <ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300 md:justify-start">
+            <li>
+              <a href="/">
+                <AiFillYoutube className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <AiFillInstagram className="w-6 h-auto" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <FaFacebook className="w-6 h-auto" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="hidden md:flex flex-col items-center space-y-4 md:col-span-12 lg:col-span-10">
+          <div className="bg-primary text-white p-3 rounded-full">
+            <AiFillHeart className="w-7 h-auto" />
+          </div>
+          <p className="font-bold italic text-dark-light">
+            Copyright © 2024. Datavica.
+          </p>
+        </div>
+      </footer>
+    </section>
+  );
+};
+
+export default Footer;

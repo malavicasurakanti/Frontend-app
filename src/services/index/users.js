b96f1383e60886dcb6c13a8e7ec8d4c2baf0ba57
Malavica Surakanti <144533175+malavicasurakanti@users.noreply.github.com>
2024-07-16 20:03:25 +0200

    Update users.js

--- src/services/index/users.js
+++ src/services/index/users.js
@@ -1,10 +1,4 @@
 import axios from "axios";
-import { API_URL } from '../services/index/Api.js';
-
-// Create an axios instance with the base URL
-const api = axios.create({
-  baseURL: API_URL
-});
 
 export const signup = async ({ name, email, password }) => {
   try {
